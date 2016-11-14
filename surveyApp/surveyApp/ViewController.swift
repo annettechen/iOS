3b2964f0ffee5db5896edd57ce644a907a2a939a
Emily Su <emilyemsu@gmail.com>
2016-11-13 23:42:27 -0500

    view controller start

--- surveyApp/surveyApp/ViewController.swift
+++ surveyApp/surveyApp/ViewController.swift
@@ -9,10 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var  questions = [String]()
+    var newQuestion: String = ""
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        questions = ["how are you","favorite color","favorite animal"]
     }
 
     override func didReceiveMemoryWarning() {
