9e981e23be0648e066ea8055460f3f06e5786a99
Annette Chen <a.chen1251@yahoo.com>
2016-12-01 23:07:39 -0500

    added nib and created profile storyboard

---
+++ surveyApp/surveyApp/SurveyCell.swift
@@ -0,0 +1,27 @@
+//
+//  SurveyCell.swift
+//  surveyApp
+//
+//  Created by Annette Chen on 12/1/16.
+//  Copyright © 2016 Annette Chen. All rights reserved.
+//
+
+import UIKit
+
+class SurveyCell: UITableViewCell {
+
+    @IBOutlet weak var surveyName: UILabel!
+    @IBOutlet weak var surveyPoints: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
--- surveyApp/surveyApp/UserProfileViewController.swift
+++ surveyApp/surveyApp/UserProfileViewController.swift
@@ -10,14 +10,19 @@ import UIKit
 
 class UserProfileViewController: UIViewController {
     
+    @IBOutlet weak var surveyTables: UITableView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Do any additional setup after loading the view.
+        let cellNib = UINib(nibName: "SurveyCell", bundle: nil)
+        surveyTables.register(cellNib, forCellReuseIdentifier: "cell")
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 }
