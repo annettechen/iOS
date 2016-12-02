a5b06557465ecbde939818847b6d08125f73376d
Annette Chen <a.chen1251@yahoo.com>
2016-12-02 13:32:41 -0500

    trying to fix the nib

---
+++ surveyApp/surveyApp/SurveyModel.swift
@@ -0,0 +1,13 @@
+//
+//  SurveyModel.swift
+//  surveyApp
+//
+//  Created by Annette Chen on 12/2/16.
+//  Copyright © 2016 Annette Chen. All rights reserved.
+//
+
+import Foundation
+
+class SurveyModel {
+    
+}
--- surveyApp/surveyApp/UserProfileViewController.swift
+++ surveyApp/surveyApp/UserProfileViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class UserProfileViewController: UIViewController {
+class UserProfileViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     @IBOutlet weak var surveyTables: UITableView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,5 +25,17 @@ class UserProfileViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath as IndexPath) as!SurveyCell
+        cell.name?.text = "Education Survey"
+        cell.points?.text = "10"
+        return cell
+    }
+
     
 }
